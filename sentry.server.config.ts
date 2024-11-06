a9692bb6f52fdcb661afa64f819c6c18812ad663
Ayushman Singh Rajawat <ayushmanrajawatv@gmail.com>
2024-11-06 10:04:09 +0530

    deployment sentry  error resolved

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,15 +1,15 @@
-// // This file configures the initialization of Sentry on the server.
-// // The config you add here will be used whenever the server handles a request.
-// // https://docs.sentry.io/platforms/javascript/guides/nextjs/
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-// import * as Sentry from "@sentry/nextjs";
+import * as Sentry from "@sentry/nextjs";
 
-// Sentry.init({
-//   dsn: "https://425d78c1e8a61b686c81b701b1c37672@o4508112017031168.ingest.de.sentry.io/4508112024436816",
+Sentry.init({
+  dsn: "https://425d78c1e8a61b686c81b701b1c37672@o4508112017031168.ingest.de.sentry.io/4508112024436816",
 
-//   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-//   tracesSampleRate: 1,
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
 
-//   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-//   debug: false,
-// });
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
